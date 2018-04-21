8874a2edd8a0c6508b8f9b4d035613a4e5beb0c0
Shipyar <olly_abraham@hotmail.com>
2018-04-21 20:23:37 +0300

    Firebase Support
    This commit contains firebase support for creating a user to Blokr and also logging in a user to blokr.
    
    Also removing some content from the Vue cli stock setup

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="login">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email Address" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button type="submit" @click.prevent="login">Login</button>
+    <p><router-link to="/Signup">Create an account </router-link></p>
+  </div>
+</template>
+
+<script>
+// Firebase Import for login systems
+import firebase from "firebase";
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        .then(msg => {
+          alert('successful connection');
+        })
+        .catch(err => {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+          alert('unsuccessful connection');
+          console.log(errorCode + errorMessage);
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer;
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+  a {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+}
+</style>
